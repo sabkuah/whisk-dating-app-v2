927847a2095af947e17393d21f8e2063612763d4
Sabrina Kuah <swkuah@gmail.com>
2021-03-05 11:22:15 -0800

    Add User/Whisk contexts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import UserState from './context/user/UserState';
+import WhiskState from './context/whisk/WhiskState';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Welcome to our new dating app!</h1>
-    </div>
+    <UserState>
+      <WhiskState>
+        <div className='App'>
+          <h1>Welcome to our new dating app!</h1>
+        </div>
+      </WhiskState>
+    </UserState>
   );
 }
 
