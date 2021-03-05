f61bbf3371914525865a2bdf1e74690808dcb5e5
Sabrina Kuah <swkuah@gmail.com>
2021-03-05 11:59:14 -0800

    Add SASS files, remove styled-components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import WhiskState from './context/whisk/WhiskState';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Navigation from './components/Navigation';
+import ChosenWhisks from './assets/layouts/ChosenWhisks';
+import UserProfile from './assets/layouts/UserProfile';
 
 function App() {
   return (
@@ -12,8 +14,14 @@ function App() {
           <Route path='/login'>
             <Login />
           </Route>
-          <Route path='/user'>{/* User Profile */}</Route>
-          <Route path='/user/whisks'>{/* User's Chosen Whisks */}</Route>
+          <Route path='/user'>
+            {/* User Profile */}
+            <UserProfile />
+          </Route>
+          <Route path='/user/whisks'>
+            {/* User's Chosen Whisks */}
+            <ChosenWhisks />
+          </Route>
           <Route path='/'>
             {/* Landing: Experiences for you */}
             <Navigation />
---
+++ src/layouts/ChosenWhisks.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ChosenWhisks = () => {
+  return <div>These are the user's chosen Whisks.</div>;
+};
+
+export default ChosenWhisks;
