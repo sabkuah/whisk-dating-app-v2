9ce04e66ca01b4b53c4b33b7e95283ba74071758
Sabrina Kuah <swkuah@gmail.com>
2021-03-08 12:16:47 -0800

    Add landing nav, #11

---
+++ src/components/landing/LandingNav.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Button, Menu, MenuItem } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const LandingNav = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Button
+        aria-controls='simple-menu'
+        aria-haspopup='true'
+        onClick={handleClick}
+      >
+        <MenuIcon />
+      </Button>
+      <Menu
+        id='simple-menu'
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default LandingNav;
--- src/layouts/Welcome.js
+++ src/layouts/Welcome.js
@@ -1,13 +1,19 @@
-import { Button, Container } from '@material-ui/core';
-import React from 'react';
+import { Button, Container, Menu, MenuItem } from '@material-ui/core';
+import React, { useState } from 'react';
 import BottomNav from '../components/landing/BottomNav';
 import Search from '../components/landing/Search';
 import Navigation from '../components/Navigation';
+import MenuIcon from '@material-ui/icons/Menu';
+import LandingNav from '../components/landing/LandingNav';
 
 const Welcome = () => {
   return (
     <Container className='welcome-page'>
-      <h2>Experiences for you</h2>
+      <LandingNav />
+      <h2>
+        Experiences <br />
+        for you
+      </h2>
       <Search />
       <BottomNav />
     </Container>
