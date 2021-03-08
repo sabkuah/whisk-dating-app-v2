f36ab82a8188c94da189dc156fd646c1fd9efc65
rachwongrw <rwrachelwong@gmail.com>
2021-03-08 12:23:31 -0800

    #8 edit to login form, remove authenticator wrapper

--- src/App.js
+++ src/App.js
@@ -5,32 +5,54 @@ import ChosenWhisks from './layouts/ChosenWhisks';
 import UserProfile from './layouts/UserProfile';
 import Welcome from './layouts/Welcome';
 import Navigation from './components/Navigation';
-import { withAuthenticator } from "@aws-amplify/ui-react"
+import { withAuthenticator, AmplifyAuthenticator, AmplifySignIn } from "@aws-amplify/ui-react"
 
 function App() {
   return (
     <UserState>
       <WhiskState>
-        <Router>
-          <Navigation/>
-          <Switch>
-            <Route path='/user'>
-              {/* User Profile */}
-              <UserProfile />
-            </Route>
-            <Route path='/user/whisks'>
-              {/* User's Chosen Whisks */}
-              <ChosenWhisks />
-            </Route>
-            <Route exact path='/'>
-              {/* Landing: Experiences for you */}
-              <Welcome />
-            </Route>
-          </Switch>
-        </Router>
+        <AmplifyAuthenticator>
+          <AmplifySignIn
+            headerText="Log In"
+            slot="sign-in"
+            usernameAlias="email"
+            hideSignUp="true"
+            formFields={[
+              {
+                type: "email",
+                label: "Email Address",
+                placeholder: "Enter your email address",
+                required: true,
+              },
+              {
+                type: "password",
+                label: "Password",
+                placeholder: "Enter your password",
+                required: true,
+              },
+            ]}
+            />
+          <Router>
+            <Navigation/>
+            <Switch>
+              <Route path='/user'>
+                {/* User Profile */}
+                <UserProfile />
+              </Route>
+              <Route path='/user/whisks'>
+                {/* User's Chosen Whisks */}
+                <ChosenWhisks />
+              </Route>
+              <Route exact path='/'>
+                {/* Landing: Experiences for you */}
+                <Welcome />
+              </Route>
+            </Switch>
+          </Router>
+        </AmplifyAuthenticator>
       </WhiskState>
     </UserState>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
