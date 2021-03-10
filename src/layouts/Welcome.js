35db1651e9ef517f81d421e0ee2e9de228f0bc4b
Sabrina Kuah <swkuah@gmail.com>
2021-03-10 12:07:59 -0800

    Fix responsiveness for mobile view

--- src/layouts/Welcome.js
+++ src/layouts/Welcome.js
@@ -7,17 +7,19 @@ import SuggestedWhisks from '../components/welcome/SuggestedWhisks';
 
 const Welcome = () => {
   return (
-    <Container className='welcome-page'>
-      {isAndroid && <BurgerNav />}
-      <div className='landing-title'>
-        <h1>
-          Experiences {isMobile && <br />}
-          for you
-        </h1>
+    <Container>
+      <div className='welcome-page'>
+        {isAndroid && <BurgerNav />}
+        <div className='landing-title'>
+          <h1>
+            Experiences {isMobile && <br />}
+            for you
+          </h1>
+        </div>
+        <Search />
+        <SuggestedWhisks />
+        {isIOS && <BottomNav />}
       </div>
-      <Search />
-      <SuggestedWhisks />
-      {isIOS && <BottomNav />}
     </Container>
   );
 };
