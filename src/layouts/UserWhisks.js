0d52cdc3fb0e246ef3e9d6c01ac0390bca582b37
rachwongrw <rwrachelwong@gmail.com>
2021-03-15 15:40:13 -0700

    update user whisk page and fix erroring out bug

--- src/layouts/UserWhisks.js
+++ src/layouts/UserWhisks.js
@@ -10,13 +10,7 @@ const UserWhisks = () => {
   const userContext = useContext(UserContext);
   const whiskContext = useContext(WhiskContext);
   const { user, cancelChooseWhisk } = userContext;
-  const {
-    whisks,
-    scanWhisks,
-    setLoadingFalse,
-    setLoadingTrue,
-    loading,
-  } = whiskContext;
+  const { whisks, scanWhisks, setLoadingFalse, setLoadingTrue, loading } = whiskContext;
   const [chosenWhisks, setChosenWhisks] = useState(null);
 
   const handleCancelWhisk = async (whiskId) => {
@@ -56,7 +50,7 @@ const UserWhisks = () => {
     return (
       <Container className='user-whisks'>
         <Grid container spacing={3}>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12} sm={6} md={6}>
             <ChosenWhisks
               whisks={chosenWhisks}
               handleCancelWhisk={handleCancelWhisk}
@@ -64,7 +58,7 @@ const UserWhisks = () => {
           </Grid>
           {
             user && 
-            <Grid item xs={12} md={6}>
+            <Grid item xs={12} sm={6} md={6}>
               <UserMatches whisks={user.Matches} />
             </Grid>
           }
