b7471ac8b80cdd5c68d8bf8931c423cbe796a298
Sabrina Kuah <swkuah@gmail.com>
2021-03-16 09:29:28 -0700

    responsiveness tweaks, #50

--- src/components/match/Messaging.js
+++ src/components/match/Messaging.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Paper, InputBase, IconButton, Avatar } from '@material-ui/core';
+import { Paper, InputBase, IconButton, Avatar, Box } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 
 const Messaging = () => {
@@ -8,18 +8,22 @@ const Messaging = () => {
       <div className='messages'>
         <div className='msg-sent'>
           <div className='wrapper'>
-            <Paper className='msg-text'>Wow I love cookies!</Paper>
+            <Paper className='msg-text'>
+              <Box m={2}>Wow I love cookies!</Box>
+            </Paper>
             <Avatar />
           </div>
         </div>
         <div className='msg-received'>
-          <div className='wrapper '>
+          <div className='wrapper'>
             <Avatar />
             <Paper className='msg-text'>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus
-              culpa commodi sequi architecto. Architecto beatae, sequi fugit
-              velit deserunt, sed atque laudantium distinctio impedit porro iure
-              alias dicta, repellat rerum.
+              <Box m={2}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Ducimus culpa commodi sequi architecto. Architecto beatae, sequi
+                fugit velit deserunt, sed atque laudantium distinctio impedit
+                porro iure alias dicta, repellat rerum.
+              </Box>
             </Paper>
           </div>
         </div>
