678d6d0e47d3462fd7e672260c9e11f0e78f0f3d
Sabrina Kuah <swkuah@gmail.com>
2021-03-15 23:08:16 -0700

    messaging styles, #50

--- src/components/match/Messaging.js
+++ src/components/match/Messaging.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { Paper, InputBase, IconButton } from '@material-ui/core';
+import { Paper, InputBase, IconButton, Avatar } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 
 const Messaging = () => {
   return (
     <Paper className='messaging'>
-      <div className='messages'></div>
+      <div className='messages'>
+        <div className='msg-sent'>
+          <div className='wrapper'>
+            <Paper className='msg-text'>Wow I love cookies!</Paper>
+            <Avatar />
+          </div>
+        </div>
+        <div className='msg-received'>
+          <div className='wrapper '>
+            <Avatar />
+            <Paper className='msg-text'>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus
+              culpa commodi sequi architecto. Architecto beatae, sequi fugit
+              velit deserunt, sed atque laudantium distinctio impedit porro iure
+              alias dicta, repellat rerum.
+            </Paper>
+          </div>
+        </div>
+      </div>
       <div className='message-input'>
         <hr />
         <div className='wrapper message-form'>
           <InputBase className='input' />
           <IconButton>
-            <SendIcon />
+            <SendIcon style={{ color: '#00d1ff' }} />
           </IconButton>
         </div>
       </div>
