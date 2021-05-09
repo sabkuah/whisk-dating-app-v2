8f4a8260210349318e970cb2490d300178359f3d
Sabrina Kuah <swkuah@gmail.com>
2021-05-09 14:02:13 -0700

    Add User model, #1

---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  fName: {
+    type: String,
+    required: true,
+  },
+  lName: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    required: true,
+  },
+  interests: [{ type: String }],
+  profileImage: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: Array,
+  },
+  phone: {
+    type: String,
+  },
+  gender: {
+    type: String,
+    enum: ['Female', 'Male', 'Non-binary'],
+  },
+  preference: {
+    type: String,
+    enum: ['Females', 'Males', 'Other'],
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  dateJoined: {
+    type: Date,
+    required: true,
+  },
+  chosenWhisks: [{ type: Schema.Types.ObjectId, ref: 'Whisk' }],
+  profileQuestionnaire: [{ type: Schema.Types.ObjectId, ref: 'Question' }],
+  matches: [{ type: Schema.Types.ObjectId, ref: 'Match' }],
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
